689ded55f460114fb6cd35efcad5b16649b4df7d
PeanutButterElf <andrew.p.council@gmail.com>
2019-03-07 10:18:54 -0500

    This is a test

--- TerminalCommandTest.py
+++ TerminalCommandTest.py
@@ -1,15 +1,18 @@
 import os
 
+
 system_message = ""
-print("Hi")
-# def pushAll(self):
-output = os.system("ping github.com")
-if output == 1:
-    print("No connection to github servers. Please connect to a network.")
-else:
-    os.system("git pull")
-    os.system("git add -A")
-    message = input("Commit Message?  :")
-    system_message = 'git commit -m "' + message + '"'
-    os.system(system_message)
-    os.system("git push")
+print("Welcome to the git pusher for your robot code!")
+while True:
+    # def pushAll(self):
+    output = os.system("ping github.com")
+    if output == 1:
+        print("No connection to github servers. Please connect to a network.")
+    else:
+        os.system("git pull")
+        os.system("git add -A")
+        message = input("Commit Message?  :")
+        system_message = 'git commit -m "' + message + '"'
+        os.system(system_message)
+        os.system("git push")
+    rerun = input("Run again?")
