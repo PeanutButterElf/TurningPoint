7436fb17a5e977b69ea551499da07a33c18138a6
PeanutButterElf <andrew.p.council@gmail.com>
2019-03-07 10:01:53 -0500

    This is another test.

--- TerminalCommandTest.py
+++ TerminalCommandTest.py
@@ -2,15 +2,12 @@ import os
 
 system_message = ""
 print("Hi")
-def pushAll(self):
-    output = os.system("ping github.com")
-    if output == 1:
-        print("No connection to github servers. Please connect to a network.")
-        return
+# def pushAll(self):
+output = os.system("ping github.com")
+if output == 1:
+    print("No connection to github servers. Please connect to a network.")
+else:
     os.system("git add -A")
     message = input("Commit Message?  :")
     system_message = 'git commit -m "' + message + '"'
 
-pushAll
-os.system(system_message)
-print("Hello")
