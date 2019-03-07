125e17d88247723dca64fbc3ab33382e56664388
PeanutButterElf <andrew.p.council@gmail.com>
2019-03-07 10:05:10 -0500

    This is the completed python script for pushing.

--- TerminalCommandTest.py
+++ TerminalCommandTest.py
@@ -7,9 +7,9 @@ output = os.system("ping github.com")
 if output == 1:
     print("No connection to github servers. Please connect to a network.")
 else:
+    os.system("git pull")
     os.system("git add -A")
     message = input("Commit Message?  :")
     system_message = 'git commit -m "' + message + '"'
     os.system(system_message)
     os.system("git push")
-
