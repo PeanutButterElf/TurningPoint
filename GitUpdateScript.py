a95868c2a1f61c097a9176fa1df445c1ea0ae352
PeanutButterElf <andrew.p.council@gmail.com>
2019-03-07 13:06:01 -0500

    Minor QOL tweaks to python git script

--- GitUpdateScript.py
+++ GitUpdateScript.py
@@ -10,12 +10,16 @@ while True:
         print("No connection to github servers. Please connect to a network.")
     else:
         os.system("git pull")
+        print("Pull process complete.\n")
         os.system("git add -A")
-        message = input("Commit Message? (nothing to escape) :\n")
+        message = input("Commit Message? (enter to escape) :\n")
         if message != "":
             system_message = 'git commit -m "' + message + '"'
             os.system(system_message)
             os.system("git push")
+            print("Push process complete.\n")
     rerun = input('Run again? (enter or "no")\n')
     if rerun == "no":
         break
+    else:
+        os.system("cls")
