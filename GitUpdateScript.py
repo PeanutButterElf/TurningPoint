408d36c27c165dd6b3422ca8977fa4f97a0242b6
PeanutButterElf <andrew.p.council@gmail.com>
2019-03-07 10:44:14 -0500

    Final final for real version of git script 3/7/19

--- GitUpdateScript.py
+++ GitUpdateScript.py
@@ -5,18 +5,17 @@ system_message = ""
 print("Welcome to the git updater for your robot code!")
 while True:
     # def pushAll(self):
-    output = os.system("ping github.com")
+    output = os.system("ping -n 2 github.com")
     if output == 1:
         print("No connection to github servers. Please connect to a network.")
     else:
         os.system("git pull")
         os.system("git add -A")
         message = input("Commit Message? (nothing to escape) :\n")
-        if message == "":
-            break
-        system_message = 'git commit -m "' + message + '"'
-        os.system(system_message)
-        os.system("git push")
+        if message != "":
+            system_message = 'git commit -m "' + message + '"'
+            os.system(system_message)
+            os.system("git push")
     rerun = input('Run again? (enter or "no")\n')
     if rerun == "no":
         break
